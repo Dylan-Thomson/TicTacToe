79900856baaae1171b655f9efe0d9a233a578378
Dylan Thomson <djthomson88@gmail.com>
2017-08-17 18:05:04 -0400

    addMove()
    -User can click on a square to add a move (x then o)
    -addMove calls checkForWinner()

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,29 @@
-alert('connected');
+// Document Ready
+$(function() {
+	addMove();
+});
+
+var turnX = true;
+var tds = $("td");
+var x = "x", o = "o";
+
+function addMove() {
+	$("td").on("click", function() {
+		if(!$(this).hasClass(x) && !$(this).hasClass(o)) {
+			if(turnX) {
+				$(this).addClass(x);
+				$(this).text(x);				
+			}
+			else {
+				$(this).addClass(o);
+				$(this).text(o);
+			}
+			turnX = !turnX;
+			checkForWinner();
+		}
+	});
+}
+
+function checkForWinner() {
+}
+
