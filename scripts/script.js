45a46feef731b00826c2bc548cc8e5634b4cae47
Dylan Thomson <djthomson88@gmail.com>
2017-08-18 15:42:47 -0400

    Rename playerMove(), remove var x and var o

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,22 @@
 // Document Ready
 $(function() {
-	playerMove();
+	initPlayerMoveListeners();
 	initResetListener();
 });
 
 var turnX = true;
 var tds = $("td");
-var x = "x", o = "o";
 
-function playerMove() {
+function initPlayerMoveListeners() {
 	$("td").on("click", function() {
-		if(!$(this).hasClass(x) && !$(this).hasClass(o)) {
+		if(!$(this).hasClass("x") && !$(this).hasClass("o")) {
 			if(turnX) {
-				$(this).addClass(x);
-				$(this).text(x);				
+				$(this).addClass("x");
+				$(this).text("x");				
 			}
 			else {
-				$(this).addClass(o);
-				$(this).text(o);
+				$(this).addClass("o");
+				$(this).text("o");
 			}
 			turnX = !turnX;
 			checkForWinner();
@@ -27,8 +26,8 @@ function playerMove() {
 
 function initResetListener() {
 	$("#reset").on("click", function() {
-		$("td").removeClass(x);
-		$("td").removeClass(o);
+		$("td").removeClass("x");
+		$("td").removeClass("o");
 		$("td").text("");
 	});
 }
