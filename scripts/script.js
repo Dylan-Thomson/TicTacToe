f08bed8727628295748e1c5b12a49f6c47293524
Dylan Thomson <djthomson88@gmail.com>
2017-08-18 19:09:37 -0400

    checkForDraw(), some refactoring
    -If win condition is not met and there are no empty squares, declare a
    draw
    -Reorganize and refactor code

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,20 @@
+var turnX = true;
+var tds = $("td");
+var winConditions = [];
+winConditions.push($("tr:nth-of-type(1) td")); // row 1
+winConditions.push($("tr:nth-of-type(2) td")); // row 2
+winConditions.push($("tr:nth-of-type(3) td")); // row 3
+winConditions.push($("td:nth-of-type(1)")); // col 1
+winConditions.push($("td:nth-of-type(2)")); // col 2
+winConditions.push($("td:nth-of-type(3)")); // col 3
+winConditions.push($("tr:nth-of-type(1) td:nth-of-type(1), tr:nth-of-type(2) td:nth-of-type(2), tr:nth-of-type(3) td:nth-of-type(3)")); // diag L->R
+winConditions.push($("tr:nth-of-type(1) td:nth-of-type(3), tr:nth-of-type(2) td:nth-of-type(2), tr:nth-of-type(3) td:nth-of-type(1)")); // diag R->L
+
 // Document Ready
 $(function() {
 	initPlayerMoveListeners();
 	initResetListener();
 });
-
-var turnX = true;
-var tds = $("td");
 
 function initPlayerMoveListeners() {
 	$("td").on("click", function() {
@@ -19,7 +28,16 @@ function initPlayerMoveListeners() {
 				$(this).text("o");
 			}
 			turnX = !turnX;
-			checkForWinner();
+
+			if(checkForWinner()) {
+				alert($(this).attr("class") + " has won!");
+				$("#reset").trigger("click");
+			}
+
+			if(checkForDraw()) {
+				alert("Draw...");
+				$("#reset").trigger("click");
+			}
 		}
 	});
 }
@@ -32,36 +50,35 @@ function initResetListener() {
 	});
 }
 
+// check each win condition to see if met
 function checkForWinner() {
-	var winConditions = [];
-	winConditions.push($("tr:nth-of-type(1) td")); // row 1
-	winConditions.push($("tr:nth-of-type(2) td")); // row 2
-	winConditions.push($("tr:nth-of-type(3) td")); // row 3
-	winConditions.push($("td:nth-of-type(1)")); // col 1
-	winConditions.push($("td:nth-of-type(2)")); // col 2
-	winConditions.push($("td:nth-of-type(3)")); // col 3
-	winConditions.push($("tr:nth-of-type(1) td:nth-of-type(1), tr:nth-of-type(2) td:nth-of-type(2), tr:nth-of-type(3) td:nth-of-type(3)")); // diag L->R
-	winConditions.push($("tr:nth-of-type(1) td:nth-of-type(3), tr:nth-of-type(2) td:nth-of-type(2), tr:nth-of-type(3) td:nth-of-type(1)")); // diag R->L
 	for(var i = 0; i < winConditions.length; i++) {
 		if(checkWinCondition(winConditions[i])) {
-			alert(winConditions[i][0].attributes.class.nodeValue + " has won!");
-			$("#reset").trigger("click");
-			break;
+			return true;
 		}
 	}
+	return false;
 }
 
+// check a win condition to see if met
 function checkWinCondition(arr) {
-	if(!arr[0].attributes.class) {
-		return false;
-	}
-	for(var i = 1; i < arr.length; i++) {
-		// check if square is empty
+	for(var i = 0; i < arr.length; i++) {
+		// current square is empty
 		if(!arr[i].attributes.class) { 
 			return false;
 		}
-		// check if current square has a different symbol than first square
-		if(arr[0].attributes.class.nodeValue !== arr[i].attributes.class.nodeValue) {
+		// current square has a different symbol than first square
+		if(i > 0 && arr[0].attributes.class.nodeValue !== arr[i].attributes.class.nodeValue) {
+			return false;
+		}
+	}
+	return true;
+}
+
+// check for empty squares
+function checkForDraw() {
+	for(var i = 0; i < tds.length; i++) {
+		if(!tds[i].attributes.class) {
 			return false;
 		}
 	}
