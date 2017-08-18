02c510f6a38fa0da67d5a9ec7285d33f7345de6c
Dylan Thomson <djthomson88@gmail.com>
2017-08-18 16:31:55 -0400

    checkForWinner() and checkWinCondition()
    -checkForWinner() builds an array of winConditions and calls
    checkWinCondition() on each one. If one is true, alert the winner
    (temporary) and reset the game
    -checkWinCondition() checks to see if a row/column/diagonal contains all
    of the same non-empty class ("x" or "o") and returns a boolean
    -Clicking #reset removes class attribute from all tds, and sets turnX to
    true

--- scripts/script.js
+++ scripts/script.js
@@ -26,13 +26,44 @@ function initPlayerMoveListeners() {
 
 function initResetListener() {
 	$("#reset").on("click", function() {
-		$("td").removeClass("x");
-		$("td").removeClass("o");
+		$("td").removeAttr("class");
 		$("td").text("");
+		turnX = true;
 	});
 }
 
 function checkForWinner() {
-	console.log($("tr:nth-of-type(2) td"));
+	var winConditions = [];
+	winConditions.push($("tr:nth-of-type(1) td")); // row 1
+	winConditions.push($("tr:nth-of-type(2) td")); // row 2
+	winConditions.push($("tr:nth-of-type(3) td")); // row 3
+	winConditions.push($("td:nth-of-type(1)")); // col 1
+	winConditions.push($("td:nth-of-type(2)")); // col 2
+	winConditions.push($("td:nth-of-type(3)")); // col 3
+	winConditions.push($("tr:nth-of-type(1) td:nth-of-type(1), tr:nth-of-type(2) td:nth-of-type(2), tr:nth-of-type(3) td:nth-of-type(3)")); // diag L->R
+	winConditions.push($("tr:nth-of-type(1) td:nth-of-type(3), tr:nth-of-type(2) td:nth-of-type(2), tr:nth-of-type(3) td:nth-of-type(1)")); // diag R->L
+	for(var i = 0; i < winConditions.length; i++) {
+		if(checkWinCondition(winConditions[i])) {
+			alert(winConditions[i][0].attributes.class.nodeValue + " has won!");
+			$("#reset").trigger("click");
+			break;
+		}
+	}
 }
 
+function checkWinCondition(arr) {
+	if(!arr[0].attributes.class) {
+		return false;
+	}
+	for(var i = 1; i < arr.length; i++) {
+		// check if square is empty
+		if(!arr[i].attributes.class) { 
+			return false;
+		}
+		// check if current square has a different symbol than first square
+		if(arr[0].attributes.class.nodeValue !== arr[i].attributes.class.nodeValue) {
+			return false;
+		}
+	}
+	return true;
+}
