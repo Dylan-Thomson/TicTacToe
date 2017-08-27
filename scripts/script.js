86cbf1b62ab2a74cb396a8a2af7660c8731cf7ed
Dylan Thomson <djthomson88@gmail.com>
2017-08-27 14:41:50 -0400

    Easy AI
    -Player can play against an AI that randomly picks an empty square. Will
    probably replace with a smarter AI because it's a little TOO easy
    -TODO do not allow player to make a move while AI is playing

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,9 @@
 var turnX = true;
 var tds = $("td");
 var winConditions = [];
+var playerX;
+var easyAI = false;
+var hardAI = false;
 
 // Document Ready
 $(function() {
@@ -9,14 +12,21 @@ $(function() {
 	initMoveListeners();
 	initResetListener();
 	initNewGameListener();
+	initSelectXOListeners();
 });
 
 function initGameModeListeners() {
 	$(".two-player").on("click", function() {
 		displayBoard();
 	});
+	$(".ai-easy").on("click", function() {
+		displaySelectXO();
+		easyAI = true;
+		hardAI = false;
+	});
 }
 
+// TODO: do not allow player to click while AI is moving
 function initMoveListeners() {
 	$("td").on("click", function() {
 		if(!$(this).hasClass("x") && !$(this).hasClass("o")) {
@@ -28,10 +38,21 @@ function initMoveListeners() {
 				$(this).addClass("o");
 				$(this).text("o");
 			}
-			turnX = !turnX;
-			gameOver($(this));
+			endTurn($(this).attr("class"));
 		}
 	});
+}
+
+function endTurn(lastMove) {
+	turnX = !turnX;
+	if(!gameOver(lastMove) && playerX !== turnX){
+		if(easyAI) {
+			easyAIMove();
+		}
+		else if(hardAI) {
+
+		}
+	}
 }
 
 function initResetListener() {
@@ -50,6 +71,23 @@ function initNewGameListener() {
 	});
 }
 
+function initSelectXOListeners() {
+	$(".select-x").on("click", function() {
+		playerX = true;
+		displayBoard();
+	});
+	$(".select-o").on("click", function() {
+		playerX = false;
+		displayBoard();
+		if(easyAI) {
+			easyAIMove();
+		}
+		else if(hardAI) {
+			hardAIMove();
+		}
+	});
+}
+
 function initWinConditions() {
 	winConditions.push($("tr:nth-of-type(1) td")); // row 1
 	winConditions.push($("tr:nth-of-type(2) td")); // row 2
@@ -64,14 +102,16 @@ function initWinConditions() {
 // check if last move ends game
 function gameOver(lastMove) {
 	if(checkForWinner()) {
-		$(".game-over-msg").text(lastMove.attr("class") + " has won!");
+		$(".game-over-msg").text(lastMove + " has won!");
 		displayGameOver();
-		return;
+		return true;
 	}
 	if(checkForDraw()) {
 		$(".game-over-msg").text("Draw...");
 		displayGameOver();
+		return true;
 	}
+	return false;
 }
 
 // check each win condition to see if met
@@ -109,20 +149,46 @@ function checkForDraw() {
 	return true;
 }
 
+function easyAIMove() {
+	console.log("EASY AI MOVING");
+	//get empty squares
+	var emptySquares = $("td").not(".x, .o");
+
+	//pick a random empty square
+	var randomEmptySquare = emptySquares[Math.floor(Math.random() * emptySquares.length)];
+
+	// make move
+	window.setTimeout(function() {
+		$("td:eq(" + tds.toArray().indexOf(randomEmptySquare) + ")").trigger("click");
+	}, 500);
+}
+
+function hardAIMove() {
+	//minimax
+}
+
 function displayBoard() {
 	$(".board").removeClass("hidden");
 	$(".select-game-mode").addClass("hidden");
 	$(".game-over").addClass("hidden");
+	$(".select-xo").addClass("hidden");
 }
 
 function displayStart() {
 	$(".select-game-mode").removeClass("hidden");
 	$(".board").addClass("hidden");
 	$(".game-over").addClass("hidden");
+	$(".select-xo").addClass("hidden");
 }
 
 function displayGameOver() {
 	$(".game-over").removeClass("hidden");
 	$(".select-game-mode").addClass("hidden");
 	$(".board").addClass("hidden");
+	$(".select-xo").addClass("hidden");
+}
+
+function displaySelectXO() {
+	$(".select-xo").removeClass("hidden");
+	$(".select-game-mode").addClass("hidden");
 }
